c955ba8d9fc53ba78e14b91356fdb9e807dcdd2b
Laurel Thorburn <laurelthorburn@gmail.com>
2022-03-19 07:20:04 -0600

    code clean up

--- script.js
+++ script.js
@@ -14,13 +14,10 @@ for (let i = 0; i < btnsOpenModal.length; i++) {
     })
 }
 
-btnCloseModal.addEventListener('click', function(){
+const closeModal = function () {
     modal.classList.add('hidden');
     overlay.classList.add('hidden');
-})
+};
 
-overlay.addEventListener('click', function(){
-    modal.classList.add('hidden');
-    overlay.classList.add('hidden');
-})
-
+btnCloseModal.addEventListener('click', closeModal)
+overlay.addEventListener('click', closeModal)
