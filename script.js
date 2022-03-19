f04b1c13822332e42df1d9c40d1e0f5a69fbcbe0
Laurel Thorburn <laurelthorburn@gmail.com>
2022-03-19 07:21:17 -0600

    Put open Modal in a fuction

--- script.js
+++ script.js
@@ -5,19 +5,21 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
 
-for (let i = 0; i < btnsOpenModal.length; i++) {
-    btnsOpenModal[i].addEventListener('click', function(){
-        console.log(`button ${[i]}: I've been clicked`)
-        //TODO: display modal, default is hidden in html
-        modal.classList.remove('hidden');
-        overlay.classList.remove('hidden');
-    })
-}
-
-const closeModal = function () {
-    modal.classList.add('hidden');
-    overlay.classList.add('hidden');
+const openModal = function () {
+  console.log(`button ${[i]}: I've been clicked`);
+  //TODO: display modal, default is hidden in html
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
 };
 
-btnCloseModal.addEventListener('click', closeModal)
-overlay.addEventListener('click', closeModal)
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  btnsOpenModal[i].addEventListener('click', openModal);
+}
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
