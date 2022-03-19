a0d20030fe34d8d0106c03e308015a5644f2f4d9
Laurel Thorburn <laurelthorburn@gmail.com>
2022-03-19 07:04:51 -0600

    Capturing html classes in js

--- script.js
+++ script.js
@@ -2,4 +2,9 @@
 
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
-const btn = document.querySelector('.close-modal');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+for (let i = 0; i < btnsOpenModal.length; i++) {
+    console.log(btnsOpenModal[i].textContent)
+}
